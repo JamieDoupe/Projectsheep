09b4d553d33b3d82d20e37ccaf71d2d05dfd829a
JamieDoupe <47975425+JamieDoupe@users.noreply.github.com>
2019-03-30 18:36:09 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,6 +4,10 @@
 
 
 
+local physics = require( "physics" )
+physics.start()
+physics.setDrawMode( "hybrid" )
+
 local Background = display.newImageRect( "Background.png", 360, 570 )
 Background.x = display.contentCenterX
 Background.y = display.contentCenterY
@@ -12,58 +16,84 @@ local platform = display.newImageRect( "platform.png", 300, 50 )
 platform.x = display.contentCenterX
 platform.y = display.contentHeight-25
 
-local Sheep = display.newImageRect( "Sheep.png", 112, 112 )
+local Sheep = display.newImageRect( "Sheep.png", 100, 112 )
 Sheep.x = display.contentCenterX
 Sheep.y = display.contentCenterY
 Sheep.alpha = 0.8
 
-local physics = require( "physics" )
-physics.start()
+local obstacle = display.newImageRect("wood block.png",50, 80)
+obstacle.x =400
+obstacle.y =230
+obstacle.speed = 1
 
-physics.addBody( platform, "static", {bounce=0} )
-physics.addBody( Sheep, "dynamic", { radius=55, bounce=0 } )
+local obstacle1 = display.newImageRect("wood block.png",50, 80)
+obstacle1.x =200
+obstacle1.y =230
+obstacle1.speed = 1
+--At this point, I have created 3 objects, the sheep, the background and the platform.
+
+local function moveobstacle()
+    if obstacle.x <-200 then
+      obstacle.x =650
+    else
+      obstacle.x =obstacle.x-obstacle.speed
+  
+    end
+    if obstacle1.x <-200 then
+      obstacle1.x =650
+    else
+      obstacle1.x =obstacle1.x-obstacle1.speed
+      end
+  end
+  
+  Runtime:addEventListener("enterFrame", moveobstacle)
+  
+
+-- I acidentally made the platform twice below
+
+--local cw, ch = display.actualContentWidth, display.actualContentHeight
+--local platform = display.newRect( display.contentCenterX, ch, 0, cw, 40 )
+--platform:setFillColor( 0.4, 0.4, 0.8 )
+--platform.objType = "platform"
+--physics.addBody( platform, "static", { bounce=0.0, friction=0.3 } )
+
+--physics.addBody( platform, "static", {bounce=0} )
 
 
-local physics = require( "physics" )
-physics.start()
-physics.setDrawMode( "hybrid" )
+physics.addBody( Sheep, "dynamic", { radius=40, bounce=0 } )
 
-local cw, ch = display.actualContentWidth, display.actualContentHeight
-local ground = display.newRect( display.contentCenterX, ch-64, cw, 64 )
-ground:setFillColor( 0.4, 0.4, 0.8 )
-ground.objType = "ground"
-physics.addBody( ground, "static", { bounce=0.0, friction=0.3 } )
+--local box={ halfWidth=40, halfHeight=10, x=100, y=70 }
+--box:setFillColor(1, 0.2,0.4)
 
-local Sheep = display.newRect( display.contentCenterX, ground.y-150, 80, 120 )
-Sheep:setFillColor( 1, 0.2, 0.4 )
-
+--This is where I have become confused, I seek to create a new box for the sheep's feet, once this box overlaps with the sheep's feet it should allow it to jump
 physics.addBody( Sheep, "dynamic",
     { density=1.0, bounce=0.0 },  -- Main body element
-    { box={ halfWidth=30, halfHeight=10, x=0, y=60 }, isSensor=true }  -- Foot sensor element
+    { shape = box, isSensor=true }  -- Foot sensor element, just what the hell does isSensor do?
 )
 Sheep.isFixedRotation = true
+
 Sheep.sensorOverlaps = 0
 
-local function pushSheep()
+local function pushSheep(event)
   if ( event.phase == "began" and Sheep.sensorOverlaps > 0 ) then
 
   local vx, vy = Sheep:getLinearVelocity()
 
  Sheep:setLinearVelocity( vx, 0 )
  Sheep:applyLinearImpulse( 0, -0.75, Sheep.x, Sheep.y )
-end
-
-Sheep:addEventListener( "tap", pushSheep )
+    end
+end 
+Runtime:addEventListener( "touch", pushSheep )
 
 local function sensorCollide( self, event )
  
-    -- Confirm that the colliding elements are the foot sensor and a ground object
-    if ( event.selfElement == 2 and event.other.objType == "ground" ) then
+    -- Confirm that the colliding elements are the foot sensor and a platform object
+    if ( event.selfElement == 2 and event.other.objType == "platform" ) then
  
-        -- Foot sensor has entered (overlapped) a ground object
+        -- Foot sensor has entered (overlapped) a platform object
         if ( event.phase == "began" ) then
             self.sensorOverlaps = self.sensorOverlaps + 1
-        -- Foot sensor has exited a ground object
+        -- Foot sensor has exited a platform object
         elseif ( event.phase == "ended" ) then
             self.sensorOverlaps = self.sensorOverlaps - 1
         end
@@ -72,5 +102,3 @@ end
 -- Associate collision handler function with character
 Sheep.collision = sensorCollide
 Sheep:addEventListener( "collision" )
-
-end
